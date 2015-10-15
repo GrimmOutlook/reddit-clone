67ee1a7193b9f9831d0fdac756252f457f055832
GrimmOutlook <grimmoutlook87@gmail.com>
2015-10-15 17:30:47 -0400

    Added ability to pair a link with a title.

--- app.js
+++ app.js
@@ -11,9 +11,14 @@ app.controller('MainCtrl', [
       {title: 'post 5', upvotes: 42}
     ];
     $scope.addPost = function(){
-      if(!$scope.title || $scope.title === "") {return;};
-      $scope.posts.push({title: $scope.title, upvotes: 0});
+      if(!$scope.title || $scope.title === "") {return;}
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
       $scope.title = '';
+      $scope.link = '';
     };
     $scope.incrementUpvotes = function(post){
       post.upvotes += 1;
